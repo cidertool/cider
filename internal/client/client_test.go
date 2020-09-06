55d155605ecb7633762de55580146038e92e27e3
Aaron Sky <aaronsky@skyaaron.com>
2020-09-06 12:38:00 -0400

    Appease the linter gods

--- internal/client/client_test.go
+++ internal/client/client_test.go
@@ -1,39 +1,39 @@
 package client
 
-import (
-	"fmt"
-	"net/http"
-	"net/http/httptest"
+// import (
+// 	"fmt"
+// 	"net/http"
+// 	"net/http/httptest"
 
-	"github.com/aaronsky/applereleaser/pkg/config"
-	"github.com/aaronsky/applereleaser/pkg/context"
-)
+// 	"github.com/aaronsky/applereleaser/pkg/config"
+// 	"github.com/aaronsky/applereleaser/pkg/context"
+// )
 
-type mockCredentials struct {
-	server *httptest.Server
-}
+// type mockCredentials struct {
+// 	server *httptest.Server
+// }
 
-func newMockContext(status int, rawResponse string) *context.Context {
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(status)
-		fmt.Fprintln(w, rawResponse)
-	}))
-	ctx := context.New(config.Project{
-		Name: "TEST",
-	})
-	ctx.Credentials = &mockCredentials{
-		server: server,
-	}
-	return ctx
-}
+// func newMockContext(status int, rawResponse string) *context.Context {
+// 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 		w.WriteHeader(status)
+// 		fmt.Fprintln(w, rawResponse)
+// 	}))
+// 	ctx := context.New(config.Project{
+// 		Name: "TEST",
+// 	})
+// 	ctx.Credentials = &mockCredentials{
+// 		server: server,
+// 	}
+// 	return ctx
+// }
 
-func (c *mockCredentials) Close() {
-	c.server.Close()
-}
+// func (c *mockCredentials) Close() {
+// 	c.server.Close()
+// }
 
-func (c *mockCredentials) Client() *http.Client {
-	return c.server.Client()
-}
+// func (c *mockCredentials) Client() *http.Client {
+// 	return c.server.Client()
+// }
 
 // func TestGetApp(t *testing.T) {
 // 	ctx := newMockContext(http.StatusOK, ``)
